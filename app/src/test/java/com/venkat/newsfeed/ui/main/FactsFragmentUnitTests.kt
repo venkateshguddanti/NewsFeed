e49cf372016091a8f0c58d57c2c130da491e40c1
ITLINFOSYS\venkatesh.g03 <venkatesh.g03@infosys.com>
2020-07-14 20:36:06 +0530

    NewsFeed: Code refraction and added unit tests for fragment

---
+++ app/src/test/java/com/venkat/newsfeed/ui/main/FactsFragmentUnitTests.kt
@@ -0,0 +1,17 @@
+package com.venkat.newsfeed.ui.main
+
+import org.junit.After
+import org.junit.Before
+
+import org.junit.Assert.*
+
+class FactsFragmentUnitTests {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+}
